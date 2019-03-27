27c7dd3c2d8b509552d72371bd130ab9230d8e8c
姚伟 <yaowei@yaoweideMacBook-Pro.local>
2019-03-27 22:36:53 +0800

    cra build can run

---
+++ postcss-sprites/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo"></div>
+          <button className="btn-success">success</button>
+          <button className="btn-error">error</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
